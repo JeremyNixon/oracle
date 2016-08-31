496e1866e38fdc9d7ba5c5df812263301609a119
JeremyNixon <jnixon2@gmail.com>
2016-08-31 15:11:06 -0700

    add sgd and batch gradient optimizers to linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -1,22 +1,27 @@
-from numpy import array, dot, transpose
-from numpy.linalg import inv
+import numpy as np
 
-def linear_regression(x_train, y_train, x_test):
+def linear_regression(x_train, y_train, lr=.0001, num_iters=1000, optimizer="sgd"):
+    x_train = np.column_stack((np.ones(len(x_train)), x_train))
+    nrow, ncol = x_train.shape
     
-    X = np.array(x_train)
-    ones = np.ones(len(X))
-    X = np.column_stack((ones,X))
-    y = np.array(y_train)
+    if optimizer == "normal":
+        pseudoinverse = np.linalg.inv(np.matmul(x_train.T, x_train))
+        weights = np.matmul(pseudoinverse, np.matmul(x_train.T, y_train))
+        return weights
     
-    Xt = transpose(X)
-    product = dot(Xt, X)
-    theInverse = inv(product)
-    w = dot(dot(theInverse, Xt), y)
-    
-    predictions = []
-    x_test = np.array(x_test)
-    for i in x_test:
-        components = w[1:] * i
-        predictions.append(sum(components) + w[0])
+    weights = .01 * np.random.randn(ncol)
+    for iteration in xrange(num_iters):
         
-    return predictions
+        if optimizer == "sgd":    
+            stochastic_sample = np.random.random_integers(0, nrow-1, nrow)
+            for index in stochastic_sample:
+                weights += lr * (y_train[index]-np.dot(x_train[index], weights)) * x_train[index]
+
+        if optimizer == "batch":
+            weights += lr * np.matmul((y_train - np.matmul(x_train, weights)), x_train)/float(len(x_train))
+
+    return weights
+
+def evaluate(x_test, weights):
+    x_test = np.column_stack((np.ones(len(x_test)), x_test))
+    return np.matmul(x_test, weights)
