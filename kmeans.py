81f317d3749fc7234e111b29559b68d469d44893
jeremynixon <jnixon2@gmail.com>
2017-08-24 09:23:49 -0700

    update k-means

--- kmeans.py
+++ kmeans.py
@@ -1,63 +1,31 @@
 import numpy as np
 import random
 
-def L2_norm(vector):
-    count = 0
-    for i in vector:
-        i = i**2
-        count += i
-    norm = np.sqrt(count)
-    return norm
-
-def cluster(x_train, means):
-    clusters = {}
-    # Assign each datapiont to a cluster based on L2 distance to cluster center
-    for datapoint in x_train:
-        best_mean_key = min([(i[0], L2_norm(datapoint-means[i[0]])) \
-                        for i in enumerate(means)], key=lambda t:t[1])[0]
-        try:
-            clusters[best_mean_key].append(datapoint)
-        except KeyError:
-            clusters[best_mean_key] = [datapoint]
-    return clusters
-
-def reevaluate_cluster_centers(means, clusters):
-    # Calculate the new cluster means with the updated cluster assignments
-    newmeans = []
-    keys = sorted(clusters.keys())
-    for k in keys:
-        newmeans.append(np.mean(clusters[k], axis = 0))
-    return newmeans
-
-def terminated(means, oldmeans):
-    # Check to see if the new means are the same as the old means
-    return (set([tuple(a) for a in means]) == set([tuple(a) for a in oldmeans]))
-
-def k_means(x_train, K):
-    # Initialize Means
-    oldmeans = random.sample(x_train, K)
-    means = random.sample(x_train, K)
-    while not terminated(means, oldmeans):
+def kmeans(x_train, k):
+    # Initialize means to random datapoints
+    oldmeans = [x_train[i] for i in [np.random.randint(len(x_train)) for i in range(k)]]
+    means = [x_train[i] for i in [np.random.randint(len(x_train)) for i in range(k)]]
+    
+    # Iterate until assignments of datapoints to means is the same between iterations
+    while(([(oldmeans[i] == means[i]).all() for i in xrange(k)]) != [True for i in xrange(k)]):
         oldmeans = means
+        assignments = {}
         
-        # Assign all points to a cluster
-        clusters = cluster(x_train, means)
-        
-        # Re-evaluate cluster centers
-        means = reevaluate_cluster_centers(oldmeans, clusters)
-        
-    cluster_list = []
-    for i, j in clusters.iteritems():
-        for k in j:
-            cluster_list.append([i, k])
-
-    final_array = []
-    dummy_array = [0]*len(cluster_list)
-    for i in range(len(cluster_list)):
-        for j in range(len(x_train)):
-            if (x_train[j] == cluster_list[i][1]).all():
-                if dummy_array[j] != 1:
-                    final_array.append([cluster_list[i][0], j, cluster_list[i][1]])
-                dummy_array[j] = 1
-                
-    return(final_array)
+        # Assign datapoints to closest mean
+        for i, datapoint in enumerate(x_train):
+            distance = float("inf")
+            for index, mean in enumerate(means):
+                d = sum((datapoint - mean)**2)
+                if d < distance:
+                    distance = d
+                    cluster = index
+            try:
+                assignments[cluster].append(i)
+            except:
+                assignments[cluster] = [i]                    
+                    
+        # Update means to be the average of the assigned datapoints
+        for i in xrange(k):
+            means[i] = sum([x_train[data_index] for data_index in assignments[i]])/len(assignments[i])
+    
+    return assignments
