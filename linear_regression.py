6154ab12e55d4e56ff8c4ca85c54291738da8228
jeremynixon <jnixon2@gmail.com>
2017-08-30 12:54:24 -0400

    update gradient comment

--- linear_regression.py
+++ linear_regression.py
@@ -14,7 +14,7 @@ def linear_regression(x_train, y_train, lr=.00001, num_iters=1000):
         diff = y_train - raw_output
         grad = np.matmul(x_train.T, diff)
         
-        #T Take Step in Direction of Gradient
+        # Take Step in Direction of Gradient
         w += lr * grad
     return w
 
